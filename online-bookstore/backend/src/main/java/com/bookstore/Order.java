36bbe470b7985dd0d1ddf21b457d6ae9aa54734d
pksvcse19752005 <vinaypydi6@gmail.com>
2025-12-04 10:30:41 +0530

    Update Order.java

--- online-bookstore/backend/src/main/java/com/bookstore/Order.java
+++ online-bookstore/backend/src/main/java/com/bookstore/Order.java
@@ -7,6 +7,15 @@ public class Order {
     private String razorpayPaymentId;
     private double totalAmount;
 
+    // NEW fields
+    private String customerName;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String phone;
+
     // Getters and setters
 
     public String getOrderId() {
@@ -36,5 +45,53 @@ public class Order {
     public void setTotalAmount(double totalAmount) {
         this.totalAmount = totalAmount;
     }
-}
 
+    public String getCustomerName() {
+        return customerName;
+    }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
